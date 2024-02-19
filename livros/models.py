cf1aed7f74253235277f69258f5d68a68a63c419
KauanMonteiro <kauanmonteirosilva789@gmail.com>
2024-02-19 09:41:10 -0300

    models, banco de dados

--- livros/models.py
+++ livros/models.py
@@ -1,3 +1,22 @@
+from pyexpat import model
+from unittest.util import _MAX_LENGTH
 from django.db import models
 
-# Create your models here.
+
+class Livro(models.Model):
+    titulo = models.CharField(max_length=65)
+    descricao = models.CharField(max_length=200, null=True, blank=True)
+    paginas = models.IntegerField(null=True, blank=True)
+    autor = models.CharField(max_length=50, null=True, blank=True)
+    emprestado = models.BooleanField(default=False)
+    data_cadastro = models.DateField(null=True, blank=True)
+    data_emprestimo = models.DateTimeField(null=True, blank=True)
+    data_devolucao = models.DateTimeField(null=True, blank=True)
+    tempo_duracao = models.DurationField(null=True, blank=True)
+    cover = models.ImageField(upload_to='livros/covers/%Y/%m/%d/')
+    
+    def __str__(self):
+        return self.titulo
+    
+    def __str__(self):
+        return self.autor
--- livros/urls.py
+++ livros/urls.py
@@ -1,7 +1,7 @@
 from django.urls import path
 from django.http import HttpResponse
-from livros.views import home
+from . import views
 
 urlpatterns = [
-    path('',home),
+    path('', views.home),
 ]
