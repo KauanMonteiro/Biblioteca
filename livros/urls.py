cd38308d51ce7523c75dacbf32288ccbd4524258
KauanMonteiro <kauanmonteirosilva789@gmail.com>
2024-03-02 14:37:29 -0300

    emprestimo e devolução

--- livros/urls.py
+++ livros/urls.py
@@ -5,4 +5,5 @@ urlpatterns = [
     path('', views.home, name='home'),
     path('livros/categoria/<int:category_id>/', views.category, name='category'),
     path('emprestar/<int:livro_id>/', views.emprestar_livro, name='emprestar_livro'),
+    path('devolver_livro/<int:livro_id>/', views.devolver_livro, name='devolver_livro'),
 ]
--- livros/views.py
+++ livros/views.py
@@ -4,6 +4,7 @@ from livros.models import Livro, Category
 from datetime import datetime, timedelta
 from django.shortcuts import redirect, get_object_or_404
 from django.urls import reverse
+from django.contrib.auth.decorators import login_required
 
 def home(request):
     livros = Livro.objects.all()
@@ -32,3 +33,14 @@ def emprestar_livro(request, livro_id):
     else:
         # Aqui você pode redirecionar para uma página de erro ou exibir uma mensagem informando que o livro já está emprestado
         return redirect(reverse('home'))
+    
+
+def devolver_livro(request, livro_id):
+    livro = get_object_or_404(Livro, pk=livro_id)
+    if livro.emprestado:
+        livro.emprestado = False
+        livro.save()
+        return redirect(reverse('home'))
+    else:
+        # Here you can redirect to an error page or display a message indicating that the book is already available
+        return HttpResponse("Este livro já está disponível para empréstimo.")
